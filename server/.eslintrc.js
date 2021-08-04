66f8612f419bdf4d061677dbe9c9932954ffbb7b
devcraft1 <ukonudennis64@gmail.com>
2021-08-04 22:52:38 +0100

    eslint

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
-    "no-console": 0,
+    "arrow-spacing": ["error", { before: true, after: true }]
   },
 };
